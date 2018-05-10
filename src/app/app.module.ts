c9e10d4691e99221c337c5ef2af4642370315c67
camell <camell@users.noreply.github.com>
2018-05-10 12:34:45 -0400

    Check app module
    Overlay not working. This branch (file-overlay) is so that I can
    incrementally add back the code from the supposedly completed branch on
    the overlay tutorial i.e. the one not working, and see if I can step
    through adding all the changes in an order that keeps the ng serve from
    going to a blank screen (which is what I have on the current
    Overlay-Screenplay branch) and make any changes that I see necessary on
    the Overlay-Screenplay branch.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
-
-import {FlexLayoutModule} from '@angular/flex-layout';
-
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MailroomMaterialModule } from './mailroom-material/mailroom-material.module';
 
 import { AppComponent } from './app.component';
 import { InboxComponent } from './inbox/inbox.component';
+
 import { FilePreviewOverlayComponent } from './file-preview-overlay/file-preview-overlay.component';
 import { FilePreviewOverlayToolbarComponent } from './file-preview-overlay-toolbar/file-preview-overlay-toolbar.component';
+import { FilePreviewOverlayService } from './file-preview-overlay.service';
+
 import { SeasonZeroForYourConsiderationComponent } from './season-zero-for-your-consideration/season-zero-for-your-consideration.component';
 
 @NgModule({
@@ -22,14 +24,20 @@ import { SeasonZeroForYourConsiderationComponent } from './season-zero-for-your-
     FilePreviewOverlayComponent,
     FilePreviewOverlayToolbarComponent,
     SeasonZeroForYourConsiderationComponent
+
   ],
   imports: [
     FlexLayoutModule,
     BrowserModule,
     BrowserAnimationsModule,
-    MailroomMaterialModule
+    MailroomMaterialModule,
+    FormsModule,
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+  FilePreviewOverlayService
+  ],
+  bootstrap: [AppComponent],
+  entryComponents: [ FilePreviewOverlayComponent ]
 })
 export class AppModule { }
