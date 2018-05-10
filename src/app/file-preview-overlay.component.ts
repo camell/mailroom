db8a60e3483570a0ea22416a4a8382f8dae393c7
camell <camell@users.noreply.github.com>
2018-05-10 14:29:44 -0400

    keep file preview overlay component file

---
+++ src/app/file-preview-overlay.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-file-preview-overlay',
+})
+export class FilePreviewOverlayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
