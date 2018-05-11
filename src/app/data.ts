1126d36b4380c4d3e37d0351c17c20592cd07f8e
camell <camell@users.noreply.github.com>
2018-05-10 21:17:50 -0400

    let dynamic expansion panels get panel names and descriptions from the data.ts file
    -expand data.ts to include the description of movie title.

--- src/app/data.ts
+++ src/app/data.ts
@@ -1,7 +1,7 @@
 export const STATIC_FILE_DATE = [
-  { name: 'AMERICAS GUEST', url: 'https://static.pexels.com/photos/371633/pexels-photo-371633.jpeg' },
-  { name: 'WASTED SPACE', url: 'https://static.pexels.com/photos/132037/pexels-photo-132037.jpeg' },
-  { name: 'SERE', url: 'https://cdn.allwallpaper.in/wallpapers/1440x900/17110/mountains-nature-trees-rivers-1440x900-wallpaper.jpg'},
-  { name: 'CHASING THE HORIZON', url: 'https://static.pexels.com/photos/35600/road-sun-rays-path.jpg' },
-  { name: 'POLYBIUS', url: 'https://static.pexels.com/photos/223022/pexels-photo-223022.jpeg' }
+  { name: 'AMERICAS GUEST', description: '', url: 'https://static.pexels.com/photos/371633/pexels-photo-371633.jpeg' },
+  { name: 'WASTED SPACE', description: '', url: 'https://static.pexels.com/photos/132037/pexels-photo-132037.jpeg' },
+  { name: 'SERE', description: '', url: 'https://cdn.allwallpaper.in/wallpapers/1440x900/17110/mountains-nature-trees-rivers-1440x900-wallpaper.jpg'},
+  { name: 'CHASING THE HORIZON', description: '', url: 'https://static.pexels.com/photos/35600/road-sun-rays-path.jpg' },
+  { name: 'POLYBIUS', description: '', url: 'https://static.pexels.com/photos/223022/pexels-photo-223022.jpeg' }
 ];
