189da25178fd21ea27055362060b4f9bef049d5c
INTERDEPENDENT <interdependent@interdedentsmbp.lan>
2018-05-16 10:31:15 -0400

    configure screenplay swiper component ts for swiper wrapper example

--- src/app/screenplay-swiper/screenplay-swiper.component.ts
+++ src/app/screenplay-swiper/screenplay-swiper.component.ts
@@ -1,15 +1,111 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { SwiperComponent, SwiperDirective, SwiperConfigInterface,
+  SwiperScrollbarInterface, SwiperPaginationInterface } from 'ngx-swiper-wrapper';
 
 @Component({
   selector: 'app-screenplay-swiper',
+  moduleId: '/src/app/screenplay-swiper/screenplay-swiper.component',
   templateUrl: './screenplay-swiper.component.html',
   styleUrls: ['./screenplay-swiper.component.css']
 })
-export class ScreenplaySwiperComponent implements OnInit {
+export class ScreenplaySwiperComponent {
+  public show: boolean = true;
 
-  constructor() { }
+  public slides = [
+    'First slide',
+    'Second slide',
+    'Third slide',
+    'Fourth slide',
+    'Fifth slide',
+    'Sixth slide'
+  ];
 
-  ngOnInit() {
+  public type: string = 'component';
+
+  public disabled: boolean = false;
+
+  public config: SwiperConfigInterface = {
+    direction: 'horizontal',
+    slidesPerView: 1,
+    keyboard: true,
+    mousewheel: true,
+    scrollbar: false,
+    navigation: true,
+    pagination: false
+  };
+
+  private scrollbar: SwiperScrollbarInterface = {
+    el: '.swiper-scrollbar',
+    hide: false,
+    draggable: true
+  };
+
+  private pagination: SwiperPaginationInterface = {
+    el: '.swiper-pagination',
+    clickable: true,
+    hideOnClick: false
+  };
+
+  @ViewChild(SwiperComponent) componentRef: SwiperComponent;
+  @ViewChild(SwiperDirective) directiveRef: SwiperDirective;
+
+  constructor() {}
+
+  public toggleType(): void {
+    this.type = (this.type === 'component') ? 'directive' : 'component';
   }
 
+  public toggleDisabled(): void {
+    this.disabled = !this.disabled;
+  }
+
+  public toggleDirection(): void {
+    this.config.direction = (this.config.direction === 'horizontal') ? 'vertical' : 'horizontal';
+  }
+
+  public toggleSlidesPerView(): void {
+    if (this.config.slidesPerView !== 1) {
+      this.config.slidesPerView = 1;
+    } else {
+      this.config.slidesPerView = 2;
+    }
+  }
+
+  public toggleOverlayControls(): void {
+    if (this.config.navigation) {
+      this.config.scrollbar = false;
+      this.config.navigation = false;
+
+      this.config.pagination = this.pagination;
+    } else if (this.config.pagination) {
+      this.config.navigation = false;
+      this.config.pagination = false;
+
+      this.config.scrollbar = this.scrollbar;
+    } else {
+      this.config.scrollbar = false;
+      this.config.pagination = false;
+
+      this.config.navigation = true;
+    }
+
+    if (this.type === 'directive') {
+      this.directiveRef.setIndex(0);
+    } else {
+      this.componentRef.directiveRef.setIndex(0);
+    }
+  }
+
+  public toggleKeyboardControl(): void {
+    this.config.keyboard = !this.config.keyboard;
+  }
+
+  public toggleMouseWheelControl(): void {
+    this.config.mousewheel = !this.config.mousewheel;
+  }
+
+  public onIndexChange(index: number): void {
+    console.log('Swiper index: ', index);
+  }
 }
+
