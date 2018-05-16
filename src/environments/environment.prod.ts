05f36af0e19958a15f2decf85bdee0a932cc779b
INTERDEPENDENT <interdependent@interdedentsmbp.lan>
2018-05-16 13:49:52 -0400

    Configure firebase in environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
   production: true
 };
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBfL_ODUr3LTUPquHWITV_WpvgQzNIplN4",
+  authDomain: "interdependent-studio-mailroom.firebaseapp.com",
+  databaseURL: "https://interdependent-studio-mailroom.firebaseio.com",
+  projectId: "interdependent-studio-mailroom",
+  storageBucket: "interdependent-studio-mailroom.appspot.com",
+  messagingSenderId: "1008471358234"
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,16 @@ export const environment = {
   production: false
 };
 
+ // Initialize Firebase
+ export const firebaseConfig = {
+  apiKey: "AIzaSyBfL_ODUr3LTUPquHWITV_WpvgQzNIplN4",
+  authDomain: "interdependent-studio-mailroom.firebaseapp.com",
+  databaseURL: "https://interdependent-studio-mailroom.firebaseio.com",
+  projectId: "interdependent-studio-mailroom",
+  storageBucket: "interdependent-studio-mailroom.appspot.com",
+  messagingSenderId: "1008471358234"
+};
+
 /*
  * In development mode, to ignore zone related error stack frames such as
  * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
