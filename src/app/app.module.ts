be8e3fc287b33d882835f1ffab0109df35204557
INTERDEPENDENT <interdependent@interdedentsmbp.lan>
2018-05-16 10:19:29 -0400

    configure app module for swipe wrapper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,18 @@ import { AppComponent } from './app.component';
 import { InboxComponent } from './inbox/inbox.component';
 import { ScreenplaySwiperComponent } from './screenplay-swiper/screenplay-swiper.component';
 
+import { SwiperModule, SwiperConfigInterface,
+  SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+
+  const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+    observer: true,
+    direction: 'horizontal',
+    threshold: 50,
+    spaceBetween: 5,
+    slidesPerView: 1,
+    centeredSlides: true
+  };
+
 @NgModule({
   declarations: [
     AppComponent,
